04f7e4ff114458c41eaf2947bcbff50acdf1b302
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2018-08-29 00:12:09 +0200

    download

---
+++ download.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#First parameter: git username
+#Second parameter: -t -> do not use tags
+
+trap exit ERR
+
+if [ "$2" != "-t" ] ; then
+	echo "USE TAGS"
+	TAG_SCITOOLS=BUILD25
+	TAG_DEEPVAR=v1.0.0
+	TAG_SCRCAT2=v1.0.0
+fi
+
+echo "--------------"
+git clone https://$1@github.com/AGILESCIENCE/AGILE-GRID-ScienceTools-Setup.git
+cd AGILE-GRID-ScienceTools-Setup
+if [ "$2" != "-t" ]; then
+        git checkout $TAG_SCITOOLS
+else
+        echo "Use master version"
+fi
+cd ..
+
+echo "--------------"
+git clone https://$1@github.com/ASTRO-EDU/DeepVar.git 
+cd DeepVar
+if [ "$2" != "-t" ]; then
+        git checkout $TAG_DEEPVAR
+else
+        echo "Use master version"
+fi
+cd ..
+
+
+echo "--------------"
+git clone https://$1@github.com/ASTRO-EDU/AGILE-GRID-scripts-cat2.git 
+cd AGILE-GRID-scripts-cat2
+if [ "$2" != "-t" ]; then
+	git checkout $TAG_SCRCAT2
+else
+	echo "Use master version"
+fi
+cd ..
+
+
